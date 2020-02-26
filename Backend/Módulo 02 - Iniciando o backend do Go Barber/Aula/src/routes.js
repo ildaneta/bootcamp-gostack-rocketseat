ce0b2a09e1f1bd1f99fe8e8f2d4a34f2bb28b030
ildasilva <ildaemanoely@hotmail.com>
2020-02-26 17:46:28 -0300

    Configurações de cadastro e autenticação de usuários

--- Backend/Módulo 02 - Iniciando o backend do Go Barber/Aula/src/routes.js
+++ Backend/Módulo 02 - Iniciando o backend do Go Barber/Aula/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (_, res) => {
-  return res.json({ message: 'Oláaaa' });
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// defining the global middleware
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
