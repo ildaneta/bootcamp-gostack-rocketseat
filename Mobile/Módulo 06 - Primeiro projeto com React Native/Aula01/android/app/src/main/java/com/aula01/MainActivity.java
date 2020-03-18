9eed574c478142fe1a5ed3c28259f4ef261e6307
ildasilva <ildaemanoely@hotmail.com>
2020-03-18 06:45:54 -0300

    feat: excluding information that had been entered manually

--- Mobile/Módulo 06 - Primeiro projeto com React Native/Aula01/android/app/src/main/java/com/aula01/MainActivity.java
+++ Mobile/Módulo 06 - Primeiro projeto com React Native/Aula01/android/app/src/main/java/com/aula01/MainActivity.java
@@ -1,9 +1,6 @@
 package com.aula01;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
   @Override
@@ -11,13 +8,4 @@ public class MainActivity extends ReactActivity {
     return "Aula01";
   }
 
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
-  }
 }
