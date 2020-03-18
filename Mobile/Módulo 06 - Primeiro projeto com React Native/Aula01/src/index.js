1a7f923de9bd135d497dd0c3d025efa4e0f5216d
ildasilva <ildaemanoely@hotmail.com>
2020-03-18 06:52:07 -0300

    feat: adding the necessary settings for the code to support react-navigation version 5

--- Mobile/Módulo 06 - Primeiro projeto com React Native/Aula01/src/index.js
+++ Mobile/Módulo 06 - Primeiro projeto com React Native/Aula01/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import {StatusBar} from 'react-native';
+import 'react-native-gesture-handler';
 import './config/ReactotronConfig';
 
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
 import Routes from './routes';
 
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="light-content" backgroundColor="#f7d5d9" />
-      <Routes />
-    </>
+    <NavigationContainer>
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#f7a5a6" />
+        <Routes />
+      </>
+    </NavigationContainer>
   );
 }
