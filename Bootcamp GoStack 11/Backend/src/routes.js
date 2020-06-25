b60a0421a97df773f7e3ff68a6c18889424d1b70
Ilda Neta <iildaemanoely@gmail.com>
2020-06-25 17:11:13 -0300

    feat: creating the new routes

--- Bootcamp GoStack 11/Backend/src/routes.js
+++ Bootcamp GoStack 11/Backend/src/routes.js
@@ -1,7 +1,65 @@
 import express from 'express';
+import { uuid } from 'uuidv4';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+const projects = [];
+
+routes.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  const results = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return response.json(results);
 });
+
+routes.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+
+  return response.json(project);
+});
+
+routes.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  // index(project) not found
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found!' });
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+routes.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found!' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
+});
+
+export default routes;
