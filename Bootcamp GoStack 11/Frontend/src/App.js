388624e25c77fa7f03572d5970ed2b2563148f81
Ilda Neta <iildaemanoely@gmail.com>
2020-06-30 09:06:11 -0300

    feat: installing packages and creating the body of boilerplate for react

---
+++ Bootcamp GoStack 11/Frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ Bootcamp GoStack 11/Frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Header() {
+  return (
+    <header>
+      <h1>ReactJS</h1>
+    </header>
+  );
+}
+
+export default Header;
