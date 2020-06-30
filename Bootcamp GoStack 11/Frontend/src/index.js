4712be3d036bc81f65c8933a9c9c880529e19a89
Ilda Neta <iildaemanoely@gmail.com>
2020-06-30 08:57:41 -0300

    feat: creating the app file

--- Bootcamp GoStack 11/Frontend/src/index.js
+++ Bootcamp GoStack 11/Frontend/src/index.js
@@ -1,4 +1,6 @@
-const soma = (a, b) => {
-  return a + b;
-};
-console.log(soma(1, 3));
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+render(<App />, document.getElementById('app'));
