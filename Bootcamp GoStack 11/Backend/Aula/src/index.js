04e2d75289a358c6fbad8de28126d1008f6b8788
Ilda Neta <iildaemanoely@gmail.com>
2020-07-01 17:32:47 -0300

    feat: changing the door and add the cors package

--- Bootcamp GoStack 11/Backend/Aula/src/index.js
+++ Bootcamp GoStack 11/Backend/Aula/src/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(4000, () => {
   console.log('Servidor iniciado!');
 });
 
