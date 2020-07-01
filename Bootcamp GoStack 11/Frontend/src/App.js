f3e5a1a1c169052bbe7997c7c8684fee066e0422
Ilda Neta <iildaemanoely@gmail.com>
2020-07-01 10:50:54 -0300

    feat: add news tags in the children component

--- Bootcamp GoStack 11/Frontend/src/App.js
+++ Bootcamp GoStack 11/Frontend/src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState([
+    'Desenvolvimento de app',
+    'Front-end web',
+  ]);
+
+  function handleAddProject() {
+    setProjects([...projects, 'Novo projeto']);
+  }
+
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <Header title="Projects" />
+      <ul>
+        {projects.map((proj) => (
+          <li key={proj}>{proj}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
   );
 }
 
