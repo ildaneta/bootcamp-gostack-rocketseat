7fc9a7cf4ed26e485d63ec66308c34ae1cea3ebf
Ilda Neta <iildaemanoely@gmail.com>
2020-07-26 15:16:15 -0300

    feat: creating the appointments routes

--- Bootcamp GoStack 11/Backend/FundamentsNode/Aula/src/routes/appointments.routes.ts
+++ Bootcamp GoStack 11/Backend/FundamentsNode/Aula/src/routes/appointments.routes.ts
@@ -1,5 +1,36 @@
 import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already boodked!' });
+  }
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return response.json(appointment);
+});
 
 export default appointmentsRouter;
