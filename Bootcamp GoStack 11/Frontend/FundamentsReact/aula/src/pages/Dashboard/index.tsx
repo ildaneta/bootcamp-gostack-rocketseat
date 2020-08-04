7196d422fd703232c1ca7555efd2abdd2c905c59
Ilda Neta <iildaemanoely@gmail.com>
2020-08-04 12:20:23 -0300

    feat: creating a new Dashboard component and your styles

---
+++ Bootcamp GoStack 11/Frontend/FundamentsReact/aula/src/pages/Dashboard/index.tsx
@@ -0,0 +1,81 @@
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
+
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    // consumir api do github
+    // salvar novo repo no estado
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/repositório repositório!');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('O autor/repositório digitado não foi encontrado!');
+    }
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={event => setNewRepo(event.target.value)}
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map(repo => (
+          <a href="teste" key={repo.full_name}>
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
+
+            <div>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ Bootcamp GoStack 11/Frontend/FundamentsReact/aula/src/pages/Dashboard/styles.ts
@@ -0,0 +1,116 @@
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+interface IFormProps {
+  hasError: boolean;
+}
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  margin-top: 80px;
+  line-height: 56px;
+`;
+
+export const Form = styled.form<IFormProps>`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+  border: ${props => (props.hasError ? '3px solid #c53030' : '3px solid #fff')};
+  border-radius: 5px;
+
+  /* ${props =>
+    props.hasError &&
+    css`
+      border-color: #c53030;
+    `} */
+
+  input {
+    flex: 1;
+    padding: 0 24px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    background: #04d361;
+    /* border-radius: 5px 5px 5px 5px; */
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+    font-size: 18px;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+
+  input,
+  button {
+    height: 70px;
+    border: 0;
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a + a {
+    margin-top: 16px;
+  }
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`;
