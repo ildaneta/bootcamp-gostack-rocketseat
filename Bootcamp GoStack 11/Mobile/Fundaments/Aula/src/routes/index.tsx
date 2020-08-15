ade36a34f40cc2688113d5b973ab3b297668a918
Ilda Neta <iildaemanoely@gmail.com>
2020-08-15 18:38:21 -0300

    feat: creating the routes file

---
+++ Bootcamp GoStack 11/Mobile/Fundaments/Aula/src/routes/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => {
+  return (
+    <Auth.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: '#312e38',
+          flex: 1,
+        },
+      }}
+    >
+      <Auth.Screen name="SignIn" component={SignIn} />
+      <Auth.Screen name="SignUp" component={SignUp} />
+    </Auth.Navigator>
+  );
+};
+
+export default AuthRoutes;
