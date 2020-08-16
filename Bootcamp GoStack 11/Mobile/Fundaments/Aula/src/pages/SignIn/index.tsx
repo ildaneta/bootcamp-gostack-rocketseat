962f8a3135d416b58db0db43b3ea03747ab56e35
Ilda Neta <iildaemanoely@gmail.com>
2020-08-16 19:26:00 -0300

    feat: creating the Sign In component and it's styles

--- Bootcamp GoStack 11/Mobile/Fundaments/Aula/src/pages/SignIn/index.tsx
+++ Bootcamp GoStack 11/Mobile/Fundaments/Aula/src/pages/SignIn/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImg} />
+
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button>Entrar</Button>
+    </Container>
+  );
 };
 
 export default SignIn;
