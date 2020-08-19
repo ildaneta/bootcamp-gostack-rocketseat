03125c7c46aa68365652fff777a2f30572fa96b9
Ilda Neta <iildaemanoely@gmail.com>
2020-08-19 19:50:20 -0300

    feat: creating the rules of the input component for the unform

--- Bootcamp GoStack 11/Mobile/Fundaments/Aula/src/components/Input/index.tsx
+++ Bootcamp GoStack 11/Mobile/Fundaments/Aula/src/components/Input/index.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  useImperativeHandle,
+  forwardRef,
+} from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 
 import { Container, TextInput, Icon } from './styles';
 
@@ -8,15 +16,84 @@ interface IInputProps extends TextInputProps {
   icon: string;
 }
 
-const Input: React.FC<IInputProps> = ({ name, icon, ...rest }: IInputProps) => (
-  <Container>
-    <Icon name={icon} size={20} color="#666360" />
+interface IInputValueReference {
+  value: string;
+}
 
-    <TextInput
-      {...rest}
-      placeholderTextColor="#666360"
-      keyboardAppearance="dark"
-    />
-  </Container>
-);
-export default Input;
+interface IInputRef {
+  focus(): void;
+}
+
+const Input: React.RefForwardingComponent<IInputRef, IInputProps> = (
+  { name, icon, ...rest }: IInputProps,
+  ref,
+) => {
+  const inputElementRef = useRef<any>(null);
+
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+  const inputValueRef = useRef<IInputValueReference>({ value: defaultValue });
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    if (inputValueRef.current.value) {
+      setIsFilled(true);
+    } else {
+      setIsFilled(false);
+    }
+  }, []);
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus();
+    },
+  }));
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      setValue(reference: any, value: string) {
+        inputValueRef.current.value = value;
+        inputElementRef.current.setNativeProps({ text: value });
+      },
+
+      clearValue() {
+        inputValueRef.current.value = '';
+        inputElementRef.current.clear();
+      },
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFocused={isFocused}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
+
+      <TextInput
+        ref={inputElementRef}
+        placeholderTextColor="#666360"
+        keyboardAppearance="dark"
+        defaultValue={defaultValue}
+        onChangeText={value => {
+          inputValueRef.current.value = value;
+        }}
+        onBlur={handleInputBlur}
+        onFocus={handleInputFocus}
+        {...rest}
+      />
+    </Container>
+  );
+};
+export default forwardRef(Input);
